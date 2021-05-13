46d94a11deb0e216d5c2f8dd802a679e47610d90
Your Name <mikaylabeelders@gmail.com>
2021-05-13 10:12:59 +0200

    final

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+#Midrand Speeedster
 speed = int(input("What is the average speed of the driver in km/h: "))
 speed_limit = int(input("What is the allowed speed on the road: "))
 points = (speed- speed_limit)/5
 
+#driver will receive a demerit for every 5 km driven over the speed limit
+
+#if driver receives more than 12 demerits, he will go straight to jail
 if speed < 60:
     print("Ok")
 else:
